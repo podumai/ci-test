e823d96e68237fece0157445212c53e923f929a0
podumai <kirillsm05@gmail.com>
2026-04-15 20:51:23 +0700

    chore: add conan2 build recipe

---
+++ conanfile.py
@@ -0,0 +1,12 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+class CiTestDeps(ConanFile):
+    settings: list[str] = ["os", "compiler", "build_type", "arch"]
+    generators: list[str] = ["CMakeDeps"]
+
+    def requirements(self) -> None:
+        self.requires("boost/1.90.0")
+
+    def layout(self) -> None:
+        cmake_layout(self)

086fc2d49e7b6e84900b3360128f045316b5568f
podumai <kirillsm05@gmail.com>
2026-04-15 23:10:14 +0700

    chore: add cmake toolchain for generators

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class CiTestDeps(ConanFile):
     settings: list[str] = ["os", "compiler", "build_type", "arch"]
-    generators: list[str] = ["CMakeDeps"]
+    generators: list[str] = ["CMakeDeps", "CMakeToolchain"]
 
     def requirements(self) -> None:
         self.requires("boost/1.90.0")
